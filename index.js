216ff54584c1925171711aee76c38ad97f280f46
KelseyRocco <kelseyroccoh@gmail.com>
2022-08-17 08:33:47 -0400

    Add function to check user input

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const num1 = Math.floor(Math.random() * 11);
 const num2 = Math.floor(Math.random() * 11);
 
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+  score = 0;
+} else if (score < 0) {
+  score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+
 const questionEl = document.getElementById("question");
 questionEl.innerText = `What is ${num1} x ${num2}?`;
+
+const correctAnswer = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAnswer = +inputEl.value;
+
+  if (userAnswer === correctAnswer) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
